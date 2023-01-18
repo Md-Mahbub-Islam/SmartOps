8e95781787ea48bd16b39dcd5e89aa3dc5e298e0
Md-Mahbub-Islam <md.m.islam@abo.fi>
2023-01-18 17:23:37 +0200

    updates

--- desktop/whisperizer.py
+++ desktop/whisperizer.py
@@ -5,15 +5,15 @@ import json
 
 model = whisper.load_model('base')
 
-def AudioTrunkizer(sound_file):
+def AudioChunkizer(sound_file):
 
     #create folder if not exists
-    if not os.path.exists('trunks'):
-        os.makedirs('trunks')
+    if not os.path.exists('chunks'):
+        os.makedirs('chunks')
 
     #delete contents of the folder
-    for files in os.listdir('trunks'):
-        os.remove('trunks/'+files)
+    for files in os.listdir('chunks'):
+        os.remove('chunks/'+files)
 
     #get duration of audio file in seconds
     sound = AudioSegment.from_file(sound_file)
@@ -23,16 +23,16 @@ def AudioTrunkizer(sound_file):
     duration = duration / 60
     print('Duration: ' + str(duration)+' minutes')
 
-    #get number of trunks
-    trunks = duration / 5
+    #get number of chunks
+    chunks = duration / 5
     
     #duration of the chunk
     d_ch = 5
 
-    nn = int(trunks)
+    nn = int(chunks)
 
 
-    for i in range(0, int(trunks)):
+    for i in range(0, int(chunks)):
         StrtMin = i*d_ch
         StrtSec = 0
         EndMin = i*d_ch + d_ch
@@ -43,22 +43,32 @@ def AudioTrunkizer(sound_file):
         # Opening file and extracting portion of it
         extract = sound[StrtTime:EndTime]
         # Saving file in required location
-        extract.export(f"trunks/US_{StrtMin}-{EndMin}.mp3", format="mp3")
+        extract.export(f"chunks/US_{StrtMin}-{EndMin}.mp3", format="mp3")
         
     #save number of chunks to json
-    with open('trunks/chunk_size.json', 'w') as f:
+    with open('chunks/chunk_size.json', 'w') as f:
         json.dump(nn, f)
+
+def Chunkscribe():
+    #load number of chunks
+    nn = 0
+    with open('chunks/chunk_size.json', 'r') as f:
+        nn = json.load(f)
+
+    print(nn)
+    for i in range(0, nn):
+        t_text = DoTranscribe(f'chunks/US_{i*5}-{i*5+5}.mp3')
+        with open('chunks/US_{i*5}-{i*5+5}.txt', 'w') as f:
+            json.dump(t_text, f)
 
 
 def DoTranscribe(sound_file, language='en'):
-    #load number of chunks
-    nn = 0
-    with open('trunks/chunk_size.json', 'r') as f:
-        nn = json.load(f)
-
-    for i in range(0, int(trunks)):
+    t_text = model.transcribe(sound_file, language='en')
+    print(t_text)
+    return t_text
         
     
 
 if __name__ == '__main__':
-    AudioTrunkizer('..\cut.wav')
+    #AudioChunkizer('..\cut.wav')
+    Chunkscribe()
