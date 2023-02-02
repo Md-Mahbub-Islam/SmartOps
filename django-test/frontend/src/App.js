77e32280bd0c272310cb12208c0a8c8f4e771f89
Md Shariful Islam <32636571+Sharif1813@users.noreply.github.com>
2023-02-02 20:27:32 +0200

    fixed an issue for routes

--- django-test/frontend/src/App.js
+++ django-test/frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Layout from './components/Home/Layout';
 import SpeechToText from './components/Home/SpeechToText';
-import RealTimeMap from '.components/Home/map.js';
+import RealTimeMap from './components/Home/map.js';
 
 
 function App() {
